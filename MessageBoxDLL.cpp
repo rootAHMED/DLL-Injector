39f2cba5d9ace51c30325e20dfcf5c67e8b13ebd
AHMED ALBALAWI <102583986+rootAHMED@users.noreply.github.com>
2025-05-07 03:36:04 +0300

    MessageBoxDLL.cpp

---
+++ MessageBoxDLL.cpp
@@ -0,0 +1,15 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        // Display a message box when the DLL is loaded
+        MessageBoxW(NULL, L"DLL successfully injected into Notepad!", L"Injection Success", MB_OK | MB_ICONINFORMATION);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
